3394820477184ba72c862044837bfa141be5ee92
Md-Galib5 <mdgalibcp05@gmail.com>
2026-04-14 18:18:41 +0600

    navbar completed

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,36 +1,54 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
+import { IoMdHome } from "react-icons/io";
+import { RiTimeLine } from "react-icons/ri";
+import { ImStatsDots } from "react-icons/im";
+import { Link, NavLink } from 'react-router';
 
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
+            <div className="
+      navbar bg-base-100 w-11/12 mx-auto
+      flex flex-col sm:flex-col md:flex-row lg:flex-row
+      items-center justify-between gap-4 py-3
+    ">
+   <div className="flex justify-center md:justify-start w-full lg:w-auto">
     <img src={logo} alt="logo" />
   </div>
-  <div className="navbar-end gap-5">
-    <button className="btn btn-success">Home</button>
-    <button className="btn btn-success">Timeline</button>
-    <button className="btn btn-success">Stats</button>
-  </div>
+<div className="
+        flex flex-row sm:flex-row md:flex-row lg:flex-row
+        gap-3 items-center justify-center
+      ">
+  
+  <NavLink 
+    to="/" 
+    className={({ isActive }) => 
+      `btn font-semibold border-none ${isActive ? "bg-[#244d3f] text-white" : "bg-white text-[#64748b]"}`
+    }
+  >
+    <IoMdHome /> Home
+  </NavLink>
+
+  <NavLink 
+    to="/timeline" 
+    className={({ isActive }) => 
+      `btn font-semibold border-none ${isActive ? "bg-[#244d3f] text-white" : "bg-white text-[#64748b]"}`
+    }
+  >
+    <RiTimeLine /> Timeline
+  </NavLink>
+
+  <NavLink 
+    to="/statspage"
+    className={({ isActive }) => 
+      `btn font-semibold border-none ${isActive ? "bg-[#244d3f] text-white" : "bg-white text-[#64748b]"}`
+    }
+  >
+    <ImStatsDots /> Stats
+  </NavLink>
+
+</div>
 </div>
         </div>
     );
